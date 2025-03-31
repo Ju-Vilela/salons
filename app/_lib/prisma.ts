c1f91bdebd8a53a9e359c9cb1efbaf49b66ffc03
Julya <86087548+Ju-Vilela@users.noreply.github.com>
2025-03-30 21:43:42 -0300

    Update prisma.ts

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,17 +1,26 @@
+// @/app/_lib/prisma.ts
 import { PrismaClient } from "@prisma/client"
 
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL + 
+          (process.env.NODE_ENV === "production" 
+            ? "?pgbouncer=true&connection_limit=5"
+            : "")
+      }
+    }
+  })
+}
+
 declare global {
-  var cachedPrisma: PrismaClient
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
-let prisma: PrismaClient
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient()
-  }
-  prisma = global.cachedPrisma
-}
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+export default prisma
 
-export const db = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma
+}
